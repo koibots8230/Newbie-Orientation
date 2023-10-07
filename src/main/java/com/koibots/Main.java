7dfa4d70137e405579d5f2efe480ff9f0bd27832
CalebNeal07 <caleboneal07@gmail.com>
2023-10-06 21:08:32 -0400

    Setup for newbie lessons

---
+++ src/main/java/com/koibots/Main.java
@@ -0,0 +1,9 @@
+package com.koibots;
+
+import java.lang.System;
+
+public final class Main {
+    public static void main(String... args) {
+        System.out.println("Welcome to the Koibots!");
+    }
+}

0e97b17632a8fc452d208763b7a298430e0164ad
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2023-10-18 15:43:06 -0400

    Exersise 01

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -4,6 +4,6 @@ import java.lang.System;
 
 public final class Main {
     public static void main(String... args) {
-        System.out.println("Welcome to the Koibots!");
+        System.out.println("Welcome to the Koibots! We have poor programing teachers, such as Jake!");
     }
 }

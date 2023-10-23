a00dfc24fcfb90fb325e21e8e0f0bf5491c4cc0b
Toby Mickelson <tmickelson@tinyfern.com>
2023-10-23 09:58:36 -0400

    complete Hello World

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -4,6 +4,6 @@ import java.lang.System;
 
 public final class Main {
     public static void main(String... args) {
-        System.out.println("Welcome to the Koibots!");
+        System.out.println("Hello, World!");
     }
 }

65966f6e7b4347eea8316728faaeca073b696855
AkshayKuchibhatla <akshaykuchibhatla@gmail.com>
2023-10-31 17:13:23 -0400

    Finished classes.md exercises.

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -3,7 +3,29 @@ package com.koibots;
 import java.lang.System;
 
 public final class Main {
-    public static void main(String... args) {
-        System.out.println("Welcome to the Koibots!");
+    public static void main(String args[]) {
+        Rectangle rectangle = new Rectangle(20,30);
+        if (rectangle.print('ƒ')) {System.out.println("Success!");}
     }
 }
+
+class Rectangle {
+        private int length;
+        private int height;
+
+        public Rectangle(int length, int height) {
+            this.length = length;
+            this.height = height;
+        }
+        
+        public boolean print(char chararcter) {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < length; j++) {
+                    System.out.print(chararcter);
+                    System.out.print(" ");
+                }
+                System.out.print("\n");
+            }
+            return true;
+        }
+    }

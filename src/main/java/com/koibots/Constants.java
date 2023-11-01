d8077d3acfe717dcfad40ba000d6c34ea2943cc8
funneralplanner <plsdontatm3@gmail.com>
2023-11-01 15:46:35 -0400

    exercise 2

---
+++ src/main/java/com/koibots/Constants.java
@@ -0,0 +1,6 @@
+package com.koibots;
+
+public final class Constants {
+    public static final int intNumber = 3;
+    public static final double DOUBLE_NUMBER = 0.5;
+}
--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,9 +1,11 @@
 package com.koibots;
 
 import java.lang.System;
+import com.koibots.Constants;
 
 public final class Main {
-    public static void main(String... args) {
-        System.out.println("automotive parts are expensive!");
+    public static void main(String[] args) {
+        System.out.println(Constants.intNumber);
+        System.out.println(Constants.DOUBLE_NUMBER);
     }
 }

be5839e2946585f2873151c426b89cd6e2d7518a
Luka Yuzvenko <lukayuzvenko@gmail.com>
2023-11-01 20:05:43 -0400

    Exercise 4? (Arrays)

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,18 +1,11 @@
 package com.koibots;
 
 import java.lang.System;
-import com.koibots.Constants; 
+// import com.koibots.Constants; 
 
 public final class Main {
     public static void main(String... args) {
-        int intNumber = 6;
-
-        if (intNumber == 3) {
-            System.out.println("intNumber is three!!");
-        } else if (intNumber % 2 == 1) {
-            System.out.println("the remainder of intNumber divided by two is ONE :O");
-        } else {
-            System.out.println("booo >:(");
-        }
+        boolean[] test = new boolean[5];
+        System.out.println(test);
     }
 }

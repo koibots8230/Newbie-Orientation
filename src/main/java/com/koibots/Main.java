85b4dbcbbc9c0c308d0e50d49850eac3f6889dc9
Luka Yuzvenko <lukayuzvenko@gmail.com>
2023-11-03 21:04:50 -0400

    Hashmap exercise

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -2,10 +2,19 @@ package com.koibots;
 
 import java.lang.System;
 // import com.koibots.Constants; 
+import java.util.HashMap;
 
 public final class Main {
     public static void main(String... args) {
-        boolean[] test = new boolean[5];
-        System.out.println(test);
+        HashMap<Integer, String> teams = new HashMap<Integer, String>();
+
+        teams.put(8230, "Koibots");
+        teams.put(1086, "Blue Cheese");
+        teams.put(9003, "TuTu Turtles");
+        teams.put(5804, "TORCH");
+        teams.put(9403, "Starstuff");
+
+        System.out.println(teams.values());
+        System.out.println(teams.keySet());
     }
 }

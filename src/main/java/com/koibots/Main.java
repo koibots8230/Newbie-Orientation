4489abe234aa4c5081ecb2b0d48b91c70b123407
Luka Yuzvenko <lukayuzvenko@gmail.com>
2023-11-07 17:05:46 -0500

    Loops Exercise

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,20 +1,65 @@
 package com.koibots;
 
 import java.lang.System;
+import java.util.Scanner;
+import java.util.Random;
 // import com.koibots.Constants; 
-import java.util.HashMap;
+// import java.util.HashMap;
 
 public final class Main {
     public static void main(String... args) {
-        HashMap<Integer, String> teams = new HashMap<Integer, String>();
+        // Countdown
 
-        teams.put(8230, "Koibots");
-        teams.put(1086, "Blue Cheese");
-        teams.put(9003, "TuTu Turtles");
-        teams.put(5804, "TORCH");
-        teams.put(9403, "Starstuff");
+        System.out.println("Countdown \n");
 
-        System.out.println(teams.values());
-        System.out.println(teams.keySet());
+        int countdown = 10;
+        while (countdown >= 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        // Generate random number list
+
+        System.out.println("\nList loop \n");
+
+        int[] numberList = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = rand.nextInt(100);
+            numberList[i] = randomNumber;
+        }
+
+        // Print random list
+
+        for (int i = 0; i < numberList.length; i++) {
+            System.out.println(numberList[i]);
+        }
+
+        // Input
+
+        System.out.println("\nType in 8230 (please?) \n");
+
+        Scanner scan = new Scanner(System.in);
+        int givenNumber = 0;
+        while (givenNumber != 8230) {
+            givenNumber = scan.nextInt();
+        }
+        scan.close();
+
+        // Random list in random order??
+
+            // I don't know where to start
+
+        // Only odd numbers of a randomly generated list
+
+        System.out.println("\nOnly odd numbers \n");
+
+        for (int i = 0; i < numberList.length; i++) {
+            if (numberList[i] % 2 == 0) {
+                continue;
+            }
+            System.out.println(numberList[i]);
+        }
     }
 }

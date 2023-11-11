3f02f900e0e48d455526bccb79ff5bcf14773369
Luka Yuzvenko <lukayuzvenko@gmail.com>
2023-11-11 09:52:09 -0500

    Extras done

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -6,42 +6,8 @@ import java.lang.System;
 // import com.koibots.Constants; 
 // import java.util.HashMap;
 
-class Toaster {
-    public int strength;
-
-    public Toaster(int strength) {
-        this.strength = strength;
-    }
-
-    public String toast(String food) {
-        if (this.strength < 1) {
-            return food;
-        } else if (this.strength > 5) {
-            return "ash";
-        } else {
-            // Normal strength
-            if (food == "bread") {
-                return "toast";
-            } else {
-                return "toasted " + food;
-            }
-        }
-    }
-}
-
-
 public final class Main {
     public static void main(String... args) {
-        Toaster niftyToaster = new Toaster(2);
-        Toaster weirdToaster = new Toaster(8);
-        Toaster brokenToaster = new Toaster(0);
-
-        System.out.println(niftyToaster.toast("waffle"));
-        System.out.println(niftyToaster.toast("bread"));
-
-        System.out.println(weirdToaster.toast("bread"));
-        System.out.println(weirdToaster.toast("entire cheese wheel"));
-
-        System.out.println(brokenToaster.toast("singular banana coin"));
+        System.out.println("Done with extras :D We don't have to use those often riiight?? :D");
     }
 }

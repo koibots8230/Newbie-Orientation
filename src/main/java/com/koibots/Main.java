b0025aa0031845756bf777ffcb06fd985191c7ab
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2023-11-11 17:45:21 -0500

    Lesson 04 completed

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,18 +1,26 @@
 package com.koibots;
-
 import java.lang.System;
-import com.koibots.Constants;
+import java.util.*;
 
-public final class Main {
+public final class Main{
     public static void main(String... args) {
-        final int intNum = 26;
-        if (intNum == 3) {
-            System.out.println("Gary's Mod 5...");
-        } else if (intNum % 2 == 1) {
-            System.out.println("Ultrakill is fun (thats a name therefore it is school appropriate).");
-        }
-        else {
-            System.out.println("Yay this is all of it");
-        }
+        String[] gamesinlibrary = {"3", "4", "too many"};
+        gamesinlibrary[2] = "too many + 1";
+        System.out.println(gamesinlibrary[2]);
+
+        HashMap<Integer, String> BlacksburgTeams = new HashMap<Integer, String>();
+
+        BlacksburgTeams.put(8230, "KoiBots");
+        BlacksburgTeams.put(401, "Copperhead Robotics");
+        BlacksburgTeams.put(1086, "Blue Cheese");
+        BlacksburgTeams.put(1793, "The Aviators");
+        BlacksburgTeams.put(5724, "Spartan Robotics");
+
+        Object[] BlacksburgTeamNums = BlacksburgTeams.keySet().toArray();
+
+        System.out.println(BlacksburgTeams.get(401));
+        System.out.println(BlacksburgTeams.get(1793));
+        System.out.println(BlacksburgTeamNums[0]);
+        System.out.println(BlacksburgTeamNums[4]);
     }
 }

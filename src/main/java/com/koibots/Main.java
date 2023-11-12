252f614bb1e54c1fc29bf7da18b8adb99d58ab78
Lucy Horner <hilucyh34@gmail.com>
2023-11-12 15:05:31 -0500

    completed steps one and two of exersize 2

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -5,5 +5,9 @@ import java.lang.System;
 public final class Main {
     public static void main(String... args) {
         System.out.println("blah blah blah");
+        
+        int intNumber =  3;
+        
+        System.out.printf("My magic number is %d%n", intNumber);
     }
 }

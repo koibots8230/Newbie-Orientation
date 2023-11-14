880c665b316493ee51b13de4f1dd19079b6f93b1
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2023-11-14 17:56:58 -0500

    Lesson 6 because i feel like its close enough but i am going insane because of this so im just going to stop for now

--- src/main/java/com/koibots/Constants.java
+++ src/main/java/com/koibots/Constants.java
@@ -1,5 +1,4 @@
 package com.koibots;
 
 public class Constants {
-    public static final int KOI_TEAM_NUM = 8230;
 }
--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -3,59 +3,81 @@ import java.lang.System;
 import java.util.Random;
 import java.util.Scanner;
 import com.koibots.Constants;
+import java.util.HashMap;
 
 public final class Main{
-    public static void main(String[] args) {
-        int x = 10;
-        while (x > 0) {
-            System.out.println(x);
-            x--;
-        }
-        System.out.println("-");
-
+    String[] reforgeModifiers = {"Keen", "Superior", "Forceful", "Broken", "Damaged", "Shoddy", "Hurtful", "Strong", "Unpleasant", "Weak", "Ruthless", "Godly", "Demonic", "Zealous"};
+    HashMap<String, String> modifierDamage = new HashMap<String, String>();
+    HashMap<String, String> modifierCritChance = new HashMap<String, String>();
+    HashMap<String, String> modifierKnockback = new HashMap<String, String>();
+    public void modifierHashMapSettup() {
+        modifierDamage.put("Keen", "+0%");
+        modifierCritChance.put("Keen", "+3%");
+        modifierKnockback.put("Keen", "+0%");
+        modifierDamage.put("Superior", "+10%");
+        modifierCritChance.put("Superior", "+3%");
+        modifierKnockback.put("Superior", "+10%");
+        modifierDamage.put("Forceful", "+0%");
+        modifierCritChance.put("Forceful", "+0%");
+        modifierKnockback.put("Forceful", "+15%");
+        modifierDamage.put("Broken", "-30%");
+        modifierCritChance.put("Broken", "+0%");
+        modifierKnockback.put("Broken", "-20%");
+        modifierDamage.put("Damaged", "-15%");
+        modifierCritChance.put("Damaged", "+0%");
+        modifierKnockback.put("Damaged", "+0%");
+        modifierDamage.put("Shoddy", "-10%");
+        modifierCritChance.put("Shoddy", "+0%");
+        modifierKnockback.put("Shoddy", "-15%");
+        modifierDamage.put("Hurtful", "+10%");
+        modifierCritChance.put("Hurtful", "+0%");
+        modifierKnockback.put("Hurtful", "+0%");
+        modifierDamage.put("Weak", "+0%");
+        modifierCritChance.put("Weak", "+0%");
+        modifierKnockback.put("Weak", "-20%");
+        modifierDamage.put("Ruthless", "+18%");
+        modifierCritChance.put("Ruthless", "+0%");
+        modifierKnockback.put("Ruthless", "-10%");
+        modifierDamage.put("Godly", "+15%");
+        modifierCritChance.put("Godly", "+5%");
+        modifierKnockback.put("Godly", "+15%");
+        modifierDamage.put("Demonic", "+15%");
+        modifierCritChance.put("Demonic", "+5%");
+        modifierKnockback.put("Demonic", "+0%");
+        modifierDamage.put("Zealous", "+0%");
+        modifierCritChance.put("Zealous", "+5%");
+        modifierKnockback.put("Zealous", "+0%");
+    }
+    public static class Zenith {
         Random rand = new Random();
-        int[] randomNums = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (int y = 0; y < 10; y++){
-            randomNums[y] = rand.nextInt(10);
-        }
-        for (int z = 0; z < 10; z++){
-            System.out.println(randomNums[z]);
-        }
-        System.out.println("-");
+        public String modifier;
+        public String damage;
+        public String critChance;
+        public String knockback;
+        public int zenithNum;
 
-        Scanner inputNum = new Scanner(System.in);
-        System.out.println("Enter the KoiBots team number");
-        while(true) {
-            System.out.println(Constants.KOI_TEAM_NUM);
-            int koiTeamNumUser = inputNum.nextInt();
-            System.out.println(koiTeamNumUser);
-            if (koiTeamNumUser != Constants.KOI_TEAM_NUM) {
-                System.out.println("That is incorrect, try again.");
-            }
-            else {
-                System.out.println("That is correct!");
-                break;
-            }
+        public Zenith(String modifier, String damage, String critChance, String knockback){
+            this.modifier = modifier;
+            this.damage = damage;
+            this.critChance = critChance;
+            this.knockback = knockback;
         }
-        System.out.println("-");
-
-        int[] plantedRandNums = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (int a = 0; a < 10; a++) {
-            int randPlantingNum = rand.nextInt(10);
-            int plantingNumPos = a;
-            plantedRandNums[plantingNumPos] = randPlantingNum;
-            System.out.println(plantedRandNums[plantingNumPos]);
+        
+        public void zenithReforger(Zenith Zenith){
+            int modifierNum = rand.nextInt(14) - 1;
+            String newModifier = reforgeModifiers[modifierNum];
+            String newDamage = modifierDamage.get(newModifier);
+            String newCritChance = modifierCritChance.get(newModifier);
+            String newKnockback = modifierKnockback.get(newModifier);
+            Zenith.modifier = newModifier;
+            Zenith.damage = newDamage;
+            Zenith.critChance = newCritChance;
+            Zenith.knockback = newKnockback;
+            System.out.println("You reforged your Zenith! It has a " + newModifier + "modifier, which gives it " + newDamage + " damage, " + newCritChance + " chance to get a critical hit, and " + newKnockback + " knockback.");
         }
-        System.out.println("-");
-
-        int[] randomList = {0, 0, 0, 0, 0};
-        for (int b = 0; b < 5; b++) {
-            int randomNum = rand.nextInt(50);
-            randomList[b] = randomNum;
-            if (randomNum % 2 == 0) {
-                continue;
-            }
-            System.out.println(randomList[b]);
-        }
+    }
+    public static void main (String[] args) {
+        Zenith zenith = new Zenith("none", "none", "none", "none");
+        zenith.zenithReforger(zenith);
     }
 }

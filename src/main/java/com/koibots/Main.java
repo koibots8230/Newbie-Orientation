547bb10f4326a4d54bc16f35da36df21d75a3134
funneralplanner <plsdontatm3@gmail.com>
2023-11-14 09:54:45 -0500

    exercise 8

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,18 +1,7 @@
 package com.koibots;
-import com.koibots.Mouse;
 
 public final class Main {
     public static void main(String [] args) {
-        Mouse bartoloMyLove = new Mouse ("Bartolo", "black", 100); //ell oh ell.
-        Mouse hippyLoser = new Mouse ("hippyLoser", "purple", 3);
-        Mouse loveInterest = new Mouse ("loveInterest", "blue", 18);
-
-        String BpublicOpinion = bartoloMyLove.publicOpinion(bartoloMyLove.name); //the B is for Bartolo
-        String HpublicOpinion = hippyLoser.publicOpinion(hippyLoser.name); //the H is for hippyLoser.
-        String LpublicOpinion = loveInterest.publicOpinion(loveInterest.name);
-        
-        System.out.println(BpublicOpinion);
-        System.out.println(HpublicOpinion);
-        System.out.println(LpublicOpinion);
+       System.out.println("im done with extras");
     }
-    }
+}

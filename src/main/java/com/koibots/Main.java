97dbc3cf449cb15719c1a52b5b1f0579a1e1a5f4
Kaylee <1009583@apsva.us>
2023-11-15 15:59:18 -0500

    Finish excersice 3.

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -5,9 +5,14 @@ import com.koibots.Constants;
 
 public final class Main {
     public static void main(String... args) {
-        int intNumber = 3;
-        System.out.println("Jake is something");
-        System.out.println(intNumber); 
-        System.out.println(Constants.DOUBLE_NUMBER);
+        int intNumber = 150;
+        System.out.println("Jake is something.");
+        if (intNumber == 3) {
+           System.out.println("Java is hard.");
+        } else if (intNumber % 2 == 1) {
+            System.out.println("Carson finally comes to a meeting!");
+        } else {
+            System.out.println("Anna is pretty.");
+        }
     }
 }

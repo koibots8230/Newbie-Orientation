a160a26e50dd25b8f85bb0bfa252da0ab54de47b
Lucy Horner <hilucyh34@gmail.com>
2023-11-24 13:45:37 -0500

    exserzie 7

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,38 +1,67 @@
 package com.koibots;
 
-
 public final class Main {
     public static void main(String... args) { 
-        Fish coelacanth = new Fish(8, 2, true);
-        boolean theyDidAte = coelacanth.eat(null);
-        System.out.println(theyDidAte);
-    }
-
-    public static class Fish {
-        public int numOfFins;
-        public int numOfeyes;
-        public boolean hasScales;
        
-        public Fish(int numOfFins, int numOfeyes, boolean hasScales) {
-            this.numOfFins = 8;
-            this.numOfeyes = 2;
-            this.hasScales = true;
-        }
-        
 
-        public boolean eat(String food) {
-            System.out.println(food);
+        int v = 10;
+      
+        while (true){
+            System.out.println(v);
+            v--;
 
-            
-            if (food == "steak" ) {
-                return true;
+            if (v == -1) {
+                break;
             }
-            return false;
         }
 
-    
        
-    }
- 
-    
+     
+       Fish coelacanth = new Fish(8, 2, true);
+       boolean theyDidAte = coelacanth.eat("steak");
+       System.out.println(theyDidAte);
+       Fish rainbowTrout = new Fish (8, 2, true);
+       boolean theyDidAteAgain = rainbowTrout.eat("bug");
+   }
+
+
+   public static class Fish {
+       public int numOfFins;
+       public int numOfeyes;
+       public boolean hasScales;
+     
+       public Fish(int numOfFins, int numOfeyes, boolean hasScales) {
+           this.numOfFins = 8;
+           this.numOfeyes = 2;
+           this.hasScales = true;
+       }
+      
+
+
+       public boolean eat(String food) {
+           System.out.println(food);
+
+           if (food == "steak" ) {
+
+            return true;
+           }
+           return false;
+
+        }
+     
+       
+
+
+  
+     
+   }
+  
 }
+
+
+
+
+
+
+
+    

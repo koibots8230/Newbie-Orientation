3c3984d0f01077a1fc58c1c2db831e5eba69cee9
Benjamin Cassidy <ben.d.cassidy@gmail.com>
2023-11-27 11:05:34 -0500

    Hello Robot

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -1,24 +1,7 @@
 package com.koibots;
 
-import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-
 public final class Main {
-    public static void main(String... args) {
-        int num = 10;
-        BufferedReader BufReader = new BufferedReader(new InputStreamReader(System.in));
-        String input = BufReader.readLine();
-
-        while (num != 0) {
-            System.out.println(num);
-            num--;
-        }
-
-        while (input != "8230") {
-            System.out.println("\n");
-            input = BufReader.readLine();
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }

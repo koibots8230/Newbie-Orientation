39909941b7e5495d122be1a4447b718bb257d524
Kaylee <1009583@apsva.us>
2023-11-29 16:05:37 -0500

    E6

--- src/main/java/com/koibots/Main.java
+++ src/main/java/com/koibots/Main.java
@@ -7,35 +7,25 @@ import java.util.Random;
 //import com.koibots.Constants;
 
 public final class Main {
+    private static Main main = new Main();
+
     public static int Number = 0;
     public static void main(String... args) {
-        int x = 10;
-        while (x >= 0) {
-            System.out.println(x);
-            x--;
-        }
-        Random rand = new Random();
-        int[] daList = {1, 10, 2, 8, 3, 4, 5, 9, 6, 7};
-        for (int i = 0; i < daList.length; i++) {
-           daList[i] = rand.nextInt(10);
-            System.out.println(daList[i]);
-        }
-        
-        // Scanner in = new Scanner(System.in);
-        // System.out.println("Enter a number:");
-        // while (true){
-        //     if (in.hasNextInt()){
-        //         Number = in.nextInt();
-        //         in.nextLine();
-        //         if (Number == 8230){
-        //             System.out.println("\ncorrect");
-        //             break;
-        //         } else {
-        //             System.out.println("\nwrong");
-        //         }
-        //     }
-        // }
-        // in.close();
+        Shangwen shangwen = main.new Shangwen("purple", true);
+        System.out.println(shangwen.Art("robin"));
+    }
 
+    class Shangwen {
+        public String hairColor; 
+        public Boolean isPretty;
+
+        Shangwen(String hairColor,Boolean isPretty) {
+            this.hairColor = hairColor;
+            this.isPretty = isPretty;
+        }
+
+        public String Art(String Bird) {
+            return Bird;
+        }
     }
 }

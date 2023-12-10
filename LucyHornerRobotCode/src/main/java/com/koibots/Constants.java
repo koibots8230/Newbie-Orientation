608fe74696a034d2e850c5c63db190de59525e7a
Lucy Horner <hilucyh34@gmail.com>
2023-12-10 15:03:55 -0500

    excersize 10

---
+++ LucyHornerRobotCode/src/main/java/com/koibots/Constants.java
@@ -0,0 +1,10 @@
+package com.koibots;
+
+public class Constants {
+    
+    public static final int MOTOR_PORT = 1;
+    public static final int MOTOR_PORT2 = 2;
+
+
+
+}
--- LucyHornerRobotCode/src/main/java/frc/robot/Robot.java
+++ LucyHornerRobotCode/src/main/java/frc/robot/Robot.java
@@ -1,53 +1,45 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.koibots.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
 public class Robot extends TimedRobot {
   
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private final CANSparkMax motor = new CANSparkMax(Constants.MOTOR_PORT, MotorType.kBrushless);
+  private final CANSparkMax motor2 = new CANSparkMax(Constants.MOTOR_PORT2, MotorType.kBrushless);
+  private final RelativeEncoder encoder = motor.getEncoder();
+    
   
   
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   
   public void robotPeriodic() {}
 
-  
-  
- 
-  
-  
-  
-
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    motor2.follow(motor);
+    motor.setIdleMode(IdleMode.kBrake);
+    motor2.setIdleMode(IdleMode.kBrake);
+    encoder.setPositionConversionFactor(2);
+    encoder.setPosition(0);
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    motor.set(.50);
+    System.out.println(encoder.getPosition());
+  
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
